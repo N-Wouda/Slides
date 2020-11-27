c773305f161dd915957b57505853bb6e65903d3b
Niels Wouda <nielswouda@gmail.com>
2020-11-27 23:46:59 +0100

    [Source] EAA slideshow

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ $# -eq 0 ]] ; then
+if [[ $# -eq 0 ]]; then
     echo 'Missing commit message.'
     exit 0
 fi
@@ -8,17 +8,17 @@ fi
 # Ensure we're on src and commit source files.
 git checkout src
 git add --all
-git commit -m $1
+git commit -m "[Source] $1"
 
 # Build assets, move into repo root
 reveal-md slides/ --static _static
-mv _static/ .
+mv _static/* .
 rm favicon.ico
 
 # Deploy assets to gh-pages
 git add --all
 git checkout gh-pages
-git commit -m $1
+git commit -m "[Assets] $1"
 
 # Return to src, remove assets
 git checkout src
