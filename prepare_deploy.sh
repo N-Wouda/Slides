5172ac7368c73a6a0fad9915e15ca674716efb90
Niels Wouda <nielswouda@gmail.com>
2020-11-28 11:59:14 +0100

    [Source] Update deploy script to simplify url structure

--- prepare_deploy.sh
+++ prepare_deploy.sh
@@ -10,8 +10,21 @@ git checkout src
 git add --all
 git commit -m "[Source] $1"
 
-# Build assets, deploy to gh-pages
+# Build assets
 reveal-md slides/ --static dist
+
+# I use a nested structure for my presentations, with each presentation in
+# a separate folder. This means each presentation can be called "index.html"
+# - the folder is already unique!
+for f in dist/*/*.html; do
+  # Based on https://stackoverflow.com/a/1322126/4316405
+  dn=$(dirname $f)
+  mv "$f" "$dn/index.html"
+done
+
 cp README.md dist/README.md
 rm dist/favicon.ico
+rm dist/index.html
+
+# Deploy to gh-pages
 ghp-import -n -m "[Assets] $1" dist
